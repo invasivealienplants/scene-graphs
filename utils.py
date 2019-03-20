9f7bb6f70dd955b5a92e08bf48f8b9685e299241
unknown <pp456@cornell.edu>
2019-03-19 21:32:34 -0400

    added nonmax suppression

--- utils.py
+++ utils.py
@@ -27,31 +27,65 @@ def get_predicate_types():
         predicates[i] = line
     return predicates
 
-'''
-Returns tuple (objs, rels) for image image_data[idx] according to pruning specifications
+def get_iou(bb1, bb2):
+    """
+    calculates IOU, bbs have format [x0,y0,x1,y1]
+    """
 
-Format: 
-objs : n_objs x 7 matrix
-    objs[:,0] : object confidence scores
-    objs[:,1] : object class type (int), according to sorted_objects.txt
-    objs[:,2] : object class score (pre-softmax, kind of irrelevant if we don't consider other classes)
-    objs[:,3:6] : bounding box in original image, as [x0,y0,x1,y1]
+    # determine the coordinates of the intersection rectangle
+    x_left = max(bb1[0], bb2[0])
+    y_top = max(bb1[1], bb2[1])
+    x_right = min(bb1[2], bb2[2])
+    y_bottom = min(bb1[3], bb2[3])
+
+    if x_right < x_left or y_bottom < y_top:
+        return 0.0
+
+    # The intersection of two axis-aligned bounding boxes is always an
+    # axis-aligned bounding box
+    intersection_area = (x_right - x_left) * (y_bottom - y_top)
+
+    # compute the area of both AABBs
+    bb1_area = (bb1[2] - bb1[0]) * (bb1[3] - bb1[1])
+    bb2_area = (bb2[2] - bb2[0]) * (bb2[3] - bb2[1])
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = intersection_area / float(bb1_area + bb2_area - intersection_area)
+    assert iou >= 0.0
+    assert iou <= 1.0
+    return iou
+
+def get_graph_matrix(idx, info, object_threshold=0.3, rel_threshold=0.0, only_connected=False, nonmax_suppress=1.0):
     
-rels : n_rels x 4 matrix
-    rels[:,0] : rel confidence score
-    rels[:,1] : subject index (i.e. objs[rel[:,1]] gives list of subjects)
-    rels[:,2] : object index (i.e. objs[rel[:,2]] gives list of objects)
-    rels[:,3] : predicate type (int), according to sorted_predicates.txt
-    an edge is given as (subject-predicate-object), eg. man-has-hat
+    '''
+    Returns tuple (objs, rels) for image image_data[idx] according to pruning specifications
+
+    Format: 
+    objs : n_objs x 7 matrix
+        objs[:,0] : object confidence scores
+        objs[:,1] : object class type (int), according to sorted_objects.txt
+        objs[:,2] : object class score (pre-softmax, kind of irrelevant if we don't consider other classes)
+        objs[:,3:6] : bounding box in original image, as [x0,y0,x1,y1]
+
+    rels : n_rels x 4 matrix
+        rels[:,0] : rel confidence score
+        rels[:,1] : subject index (i.e. objs[rel[:,1]] gives list of subjects)
+        rels[:,2] : object index (i.e. objs[rel[:,2]] gives list of objects)
+        rels[:,3] : predicate type (int), according to sorted_predicates.txt
+        an edge is given as (subject-predicate-object), eg. man-has-hat
+
+    object_threshold determines cutoff score for object confidence - only objects with confidence score > object_threshold are included
+    rel_threshold determines score for rel confidence
+    info : (image_data,graphs,objects,predicates)
+    only_connected (slight misnomer)
+        when True will only return nodes that have degree > 0
+        else includes isolated nodes
+    nonmax_suppress : determines cutoff IoU for non-max suppression : 
+        if two boxes have IoU > nonmax_suppress, only keeps box with higher confidence score
+    '''
     
-object_threshold determines cutoff score for object confidence - only objects with confidence score > object_threshold are included
-rel_threshold determines score for rel confidence
-info : (image_data,graphs,objects,predicates)
-only_connected (slight misnomer)
-    when True will only return nodes that have degree > 0
-    else includes isolated nodes
-'''
-def get_graph_matrix(idx, info, object_threshold=0.3, rel_threshold=0.0, only_connected=False):
     image_data,graphs,objects,predicates = info
     
     image = image_data[graphs['idx'][idx]]
@@ -62,19 +96,30 @@ def get_graph_matrix(idx, info, object_threshold=0.3, rel_threshold=0.0, only_co
     objs = graphs['objs'][idx][:n_objs]
     rels = graphs['rels'][idx][:n_rels]
     
+    order = np.flip(np.argsort(objs[:,0]),0)
+    objs = objs[order]
+    order = np.flip(np.argsort(rels[:,0]),0)
+    rels = rels[order]
+    
     # add nodes to graph
     objects_idx = set([])
+    past_bboxs = [] # for non max suppression
     for idx,obj in enumerate(objs):
         obj_score = obj[0]
+        skip = False
+        for bbox in past_bboxs:
+            if get_iou(bbox,obj[3:]) > nonmax_suppress:
+                skip = True
+        if skip:
+            continue
         if obj_score > object_threshold:
             object_class_idx = int(obj[1])
             objects_idx.add(idx)
+            past_bboxs.append(obj[3:])
 
     # add edges to graph
     edges_to_add = set([])
     connected_nodes = set([])
-    order = np.flip(np.argsort(rels[:,0]),0)
-    rels = rels[order]
     contained_rels = set([])
     for idx,rel in enumerate(rels):
         if rel[0] > rel_threshold:
@@ -115,7 +160,7 @@ def get_graph_matrix(idx, info, object_threshold=0.3, rel_threshold=0.0, only_co
 # no self loops, multi-edges
 # object, rel thresholds determine what score cutoff to use in pruning
 # currently NOT pruning nodes without edges
-def visualize(idx, info, object_threshold=0.3, rel_threshold=0.0, only_connected=False):
+def visualize(idx, info, object_threshold=0.3, rel_threshold=0.0, only_connected=False, nonmax_suppress=1.0):
     
     image_data,graphs,objects,predicates = info
 
@@ -129,7 +174,7 @@ def visualize(idx, info, object_threshold=0.3, rel_threshold=0.0, only_connected
     h,w = im.shape[0],im.shape[1]
     ax.imshow(im)
     
-    objs,rels = get_graph_matrix(idx,info,object_threshold,rel_threshold,only_connected)
+    objs,rels = get_graph_matrix(idx,info,object_threshold,rel_threshold,only_connected,nonmax_suppress)
     
     for object_instance in objs:
         rect = patches.Rectangle((int(object_instance[3]),int(object_instance[4])),int(object_instance[5]-object_instance[3]),int(object_instance[6]-object_instance[4]),linewidth=2,edgecolor='r',facecolor='none')
